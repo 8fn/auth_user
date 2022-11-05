b8f63f671ffcb72fb8ab93b6746bc5ce47a37ac6
Tanjil <tanjilkh@gmail.com>
2022-11-05 15:58:33 +0000

    :zap: Login POST method

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.json())
+app.use(express.json());
+const bcrypt = require('bcrypt');
 
 users = []
 
@@ -8,11 +9,35 @@ app.get('/users', (req, res) => {
     res.json(users)
 })
 
-app.post('/users', (req, res) => {
-    const user = {name: req.body.name, password: req.body.password}
-    users.push(user)
-    res.status(201).send()
+app.post('/users', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        console.log(salt);
+        console.log(hashPassword);
+        const user = {name: req.body.name, password: hashPassword}
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+   
 })
 
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name);
+    if(user == null){
+        return res.status(400).send('Cannot find user')
+    }
+    try {
+       if(await bcrypt.compare(req.body.password, user.password)){
+        res.send('Success')
+       } else {
+        res.send('Not allowed')
+       }
+    } catch (error) {
+        res.status(500).send()
+    }
+})
 console.log('Server running!')
 app.listen(3000, 'localhost')
